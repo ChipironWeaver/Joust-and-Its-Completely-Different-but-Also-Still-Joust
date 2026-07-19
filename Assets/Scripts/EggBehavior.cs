3e82f81a4ea356a174201f16da01a539c3bbcb3a
Lowen <lowen.tisserand@gmail.com>
2026-07-19 22:59:10 +0200

    Finished Egg Behiavior

--- Assets/Scripts/EggBehavior.cs
+++ Assets/Scripts/EggBehavior.cs
@@ -1,35 +1,48 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class EggBehavior : EnemyBehavior
 {
     [SerializeField] private int _pointAmount;
+    [SerializeField] private float _spawnInvulnerabilityTime;
     [SerializeField] private float _enemySpawnTime;
     [SerializeField] private UnityEvent _spawnEvent;
     private Rigidbody2D _rigidbody2D;
+    private float _lifeTime;
     public void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         Spawn();
     }
+
+    private void Update()
+    {
+        _lifeTime += Time.deltaTime;
+        if (_lifeTime >= _enemySpawnTime)
+        {
+            _spawnEvent.Invoke();
+            _lifeTime = 0;
+            Death();
+        }
+    }
+
     public override AttackResult EnemyDuel(GameObject player)
     {
-        //Give points
-        Death();
-        return AttackResult.EnemyDeath;
+        if(_lifeTime >= _spawnInvulnerabilityTime)
+        {
+            Death();
+            return AttackResult.EnemyDeath;
+            //Give points
+        }
+        return AttackResult.None;
     }
 
     public override void Spawn()
     {
         Vector2 direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         direction.Normalize();
-        Invoke(nameof(SpawnEnemy) , _enemySpawnTime);
-    }
-
-    public void SpawnEnemy()
-    {
-        _spawnEvent.Invoke();
-        Death();
     }
     
     public override void Death()
